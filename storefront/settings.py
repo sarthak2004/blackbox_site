79cfa390a0b8f2a74f6ace454bb759a82e0e185c
[sarthak2004] <sarthakishu2004@gmail.com>
2022-12-01 21:22:48 +0530

    something

--- storefront/settings.py
+++ storefront/settings.py
@@ -54,9 +54,7 @@ ROOT_URLCONF = 'storefront.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, "frontend/build")
-        ],
+        'DIRS': [os.path.join(BASE_DIR, "frontend/build")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
