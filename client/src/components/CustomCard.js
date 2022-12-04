eae42087949af781a90253a6696875d93408e6a3
[sarthak2004] <sarthakishu2004@gmail.com>
2022-12-04 14:39:47 +0530

    frontend

--- client/src/components/CustomCard.js
+++ client/src/components/CustomCard.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
+import {CardActionArea } from '@mui/material';
+import './CustomCard.css';
+
 
 function CustomCard(props) {
   return (
+    <div className='sizechange'>
     <Card>
+      <CardActionArea>
         <Card.Img variant="top" src={props.image} />
         <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         </Card.Body>
+      </CardActionArea>
     </Card>
+  </div>
   )
+  
 }
 
 export default CustomCard
